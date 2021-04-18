792f11a85cdae8bb7bf9995d8460effb80310668
卫建文 <booldesign@163.com>
2021-04-18 11:01:23 +0800

    color package

--- shutdown/shutdown.go
+++ shutdown/shutdown.go
@@ -13,6 +13,14 @@ import (
  * @Desc: 优雅关闭
  */
 
+/*
+SIGINT 值:2 说明:用户发送INTR字符(Ctrl+C)触发
+SIGTERM 值:15 说明:结束程序(可以被捕获、阻塞或忽略)
+
+golang中对信号的处理主要使用os/signal包中的两个方法：
+* notify方法用来监听收到的信号
+* stop方法用来取消监听
+*/
 // Hook a graceful shutdown hook, default with signals of SIGINT and SIGTERM
 type Hook interface {
 	WithSignals(signals ...syscall.Signal) Hook // WithSignals add more signals into hook
@@ -30,7 +38,7 @@ func NewHook() Hook {
 		singleChan: make(chan os.Signal, 1),
 	}
 
-	return hook.WithSignals(syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+	return hook.WithSignals(syscall.SIGINT, syscall.SIGTERM)
 }
 
 func (h *hook) WithSignals(signals ...syscall.Signal) Hook {
