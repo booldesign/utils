9dc2405cd210a2d81f6a2bd88b58b04da8345e2c
卫建文 <booldesign@163.com>
2021-04-18 22:03:49 +0800

    add logger

--- go.mod
+++ go.mod
@@ -4,6 +4,18 @@ go 1.14
 
 require (
 	github.com/json-iterator/go v1.1.10
+	github.com/kr/text v0.2.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20210317152858-513c2a44f670
+	golang.org/x/lint v0.0.0-20191125180803-fdd1cda4f05f // indirect
+	golang.org/x/tools v0.0.0-20191216173652-a0e659d51361 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
---
+++ logger/zap.go
@@ -0,0 +1,109 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+/**
+ * @Author: BoolDesign
+ * @Email: booldesign@163.com
+ * @Date: 2021/2/24 17:30
+ * @Desc: 日志 特别谢鸣：https://www.liwenzhou.com/posts/Go/zap/
+ */
+
+const DefaultLogPath = "./logs/micro.log"
+
+// Option custom setup config
+type Option func(*option)
+
+type option struct {
+	level   zapcore.Level
+	logPath string
+}
+
+// WithDebugLevel only greater than 'level' will output
+func WithDebugLevel() Option {
+	return func(opt *option) {
+		opt.level = zapcore.DebugLevel
+	}
+}
+
+// WithInfoLevel only greater than 'level' will output
+func WithInfoLevel() Option {
+	return func(opt *option) {
+		opt.level = zapcore.InfoLevel
+	}
+}
+
+// WithWarnLevel only greater than 'level' will output
+func WithWarnLevel() Option {
+	return func(opt *option) {
+		opt.level = zapcore.WarnLevel
+	}
+}
+
+// WithErrorLevel only greater than 'level' will output
+func WithErrorLevel() Option {
+	return func(opt *option) {
+		opt.level = zapcore.ErrorLevel
+	}
+}
+
+func WithLogPath(path string) Option {
+	return func(opt *option) {
+		opt.logPath = path
+	}
+}
+
+// 初始化 logger
+func NewJSONLogger(opts ...Option) *zap.SugaredLogger {
+	opt := &option{level: zapcore.InfoLevel, logPath: DefaultLogPath}
+	for _, f := range opts {
+		f(opt)
+	}
+	syncWriter := getLogWriter(opt.logPath)
+	encoder := zapcore.NewJSONEncoder(getEncoder())
+	core := zapcore.NewCore(
+		encoder,
+		syncWriter,
+		zap.NewAtomicLevelAt(opt.level),
+	)
+
+	log := zap.New(
+		core,
+		zap.AddCaller(), // 将调用函数信息记录到日志中
+		zap.AddCallerSkip(1),
+	)
+
+	return log.Sugar()
+}
+
+// WriterSyncer ：指定日志将写到哪里去, zap中加入Lumberjack支持
+func getLogWriter(logPath string) zapcore.WriteSyncer {
+	return zapcore.AddSync(
+		// 日志切割归档功能，使用第三方库Lumberjack来实现。
+		&lumberjack.Logger{
+			Filename:   logPath, // 日志文件名称
+			MaxSize:    128,     // MB
+			MaxAge:     30,      // 保留旧文件的最大天数
+			MaxBackups: 300,     // 保留旧文件的最大个数
+			LocalTime:  true,    // 使用本地时间
+			Compress:   true,    // 是否启用压缩
+		},
+	)
+}
+
+// 编码器
+func getEncoder() zapcore.EncoderConfig {
+	// 编码
+	encoderConfig := zap.NewProductionEncoderConfig()
+	// 时间格式
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	//Level序列化为全大写字符串
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.LineEnding = zapcore.DefaultLineEnding
+
+	return encoderConfig
+}
